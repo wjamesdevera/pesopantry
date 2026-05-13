faeffe522854054c6670e3abe7cd706d24120eba
Lec <150683104+Leckii@users.noreply.github.com>
2026-05-13 16:15:06 +0800

    Login redirection when accessing the app

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'PesoPantry',
       theme: AppTheme.lightTheme(),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
+      initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
